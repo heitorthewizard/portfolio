dd84d109e032c44080a9b775fdd9c988f5fb2a05
heitorthewizard <heitor.mb27@gmail.com>
2022-09-18 20:30:18 -0300

    mobile style pretty much done

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,7 +1,7 @@
 async function main() {
   // nav menu
-  const navMenu = document.querySelector(".show-menus-button");
-  const menuButtons = document.querySelector(".menu-buttons");
+  const navMenu = document.querySelector(".show_menus_button");
+  const menuButtons = document.querySelector(".menu_buttons");
 
   // show menu buttons when clicked
   navMenu.addEventListener("click", () => {
@@ -13,58 +13,63 @@ async function main() {
   const skills = [
     {
       name: "HTML",
-      percentage: "77%",
+      percentage: "70%",
       imgPath: "./assets/images/html5.svg",
     },
     {
       name: "CSS",
-      percentage: "68%",
+      percentage: "60%",
       imgPath: "./assets/images/css3.svg",
     },
     {
       name: "JavaScript",
-      percentage: "84%",
+      percentage: "80%",
       imgPath: "./assets/images/javascript.svg",
     },
     {
       name: "Node JS",
-      percentage: "72%",
+      percentage: "70%",
       imgPath: "./assets/images/nodejs.svg",
     },
     {
       name: "React JS",
-      percentage: "22%",
+      percentage: "20%",
       imgPath: "./assets/images/react.svg",
     },
     {
       name: "Python",
-      percentage: "73%",
+      percentage: "75%",
       imgPath: "./assets/images/python.svg",
     },
     {
       name: "MySQL",
-      percentage: "18%",
+      percentage: "20%",
       imgPath: "./assets/images/mysql.svg",
+    },
+    {
+      name: "MongoDB",
+      percentage: "20%",
+      imgPath: "./assets/images/mongodb.svg",
     },
   ];
 
   skills.forEach((el) => {
-    const skillElements = document.querySelector("#skills");
+    const skillElements = document.querySelector(".skills_container");
 
     skillElements.innerHTML += `
-      <div class="skill-data">
-        <div class="data-container">
+      <div class="skill_data">
+        <div class="data_container">
             <img src="${el.imgPath}" alt="${el.name} logo">
               <span>${el.name}</span>
               <span class="percentage">${el.percentage}</span>
         </div>
-        <div class="progress-bar"></div>
+        <div class="progress_bar"></div>
       </div>
     `;
   });
 
-  // set skills progress bar percentages 
-  const skillsData = document.querySelectorAll(".skill-data");
+  // set skills progress bar percentages
+  const skillsData = document.querySelectorAll(".skill_data");
 
   skillsData.forEach((el) => {
     const percentage = el.children[0].children[2].textContent;
@@ -93,9 +98,54 @@ async function main() {
       (error) => (reposElement.textContent = `Could not fetch verse: ${error}`)
     );
 
-  repos.forEach((obj) => {
-    reposElement.innerHTML += `<li><a href="${obj.url}" target="_blank">${obj.name}</a><li>`;
+  // displaying 6 elements
+  repos.forEach((obj, idx) => {
+    if (idx < 6) {
+      reposElement.innerHTML += `
+      <li>
+        <a href="${obj.url}" target="_blank">${obj.name}</a>
+      </li>
+      `;
+    } else {
+      reposElement.innerHTML += `
+      <li class="none">
+        <a href="${obj.url}" target="_blank">${obj.name}</a>
+      </li>
+      `;
+    }
   });
+
+  // displaying all elements and toggling back to 6 when clicked
+  const showAndHideReposButton = document.querySelector("#show_and_hide_button");
+  showAndHideReposButton.addEventListener("click", showMoreRepos);
+
+  function showMoreRepos() {
+    this.removeEventListener("click", showMoreRepos);
+    this.addEventListener("click", showLessRepos);
+    this.textContent = "Show Less";
+
+    const allRepos = document.querySelectorAll(".repos > li");
+
+    allRepos.forEach((el) => {
+      if (el.classList.contains("none")) {
+        el.classList.toggle("none");
+      }
+    });
+  }
+
+  function showLessRepos() {
+    this.removeEventListener("click", showLessRepos);
+    this.addEventListener("click", showMoreRepos);
+    this.textContent = "Show More";
+
+    const allRepos = document.querySelectorAll(".repos > li");
+
+    allRepos.forEach((el, idx) => {
+      if (idx >= 6) {
+        el.classList.add("none");
+      }
+    });
+  }
 }
 
 main();
