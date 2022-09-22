365cd42dd7858f877dd2a3de8c6cb67e8e5062b0
heitorthewizard <heitor.mb27@gmail.com>
2022-09-22 02:00:25 -0300

    fixed some miss writting in portuguese and improved the texts

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,5 +1,5 @@
 // variables that cannot be rewritten when main function is
-// called again Org the switching languages function
+// called again when switching languages
 const app = document.querySelector("#app");
 const englishPage = app.innerHTML;
 let language = "en";
@@ -25,7 +25,7 @@ async function main() {
     pageProgressBar.style.width = `${scrolledPercentage}%`;
   });
 
-  // data to be render on the web page
+  // skills data to be render on the web page
   const skills = [
     {
       name: "HTML",
@@ -417,57 +417,62 @@ async function main() {
             Deixe-me contar um pouco sobre a história de como
             eu me apaixonei pelo <em>desenvolvimento de software</em>.
             <br />
-              Em 2019 eu ainda estava no ensino médio e trabalhando ao mesmo
-              tempo que como <em>professor de Inglês</em> na mesma instituição
-              onde eu estudei Inglês. Eu também estava estudando
+              Em 2019 eu ainda estava no ensino médio e trabalhando como 
+              <em>professor de Inglês</em> na mesma instituição
+              onde eu cursei a língua. Eu também estava estudando
               <em>finanças</em>, <em>gestão de negócios</em> e
-              <em>economia</em> por se divertir. eu até cheguei a brincar um pouco
-              no <em>mercado de ações</em> seguindo
-              <em>Método de Warren Buffet</em>, eu queria começar a colocar
+              <em>economia</em> por diversão. Cheguei a brincar um pouco
+              no <em>mercado de ações</em> seguindo a metodologia 
+              de <em>Warren Buffet</em>, estava começando a colocar
               dinheiro para trabalhar para mim.
             </p>
 
             <p>
-              Eu queria muito
-              empreender em algo sustentável que durasse vários anos, mas não tinha
+              Eu também queria muito empreender em algo lucrativo e que 
+              perdurasse por vários anos, porém, não tinha
               ideia do que fazer, estava meio perdido. Em 2020, na mesma época
               em que a pandemia começou, uma familiar me deu um notebook para que
-              eu pudesse estudar com mais qualidade.
+              eu pudesse estudar com melhores condições.
             </p>
 
             <p>
-              Então um dia eu
-              estava olhando para o computador imerso em meus pensamentos até
-              que tive uma epifania de como o computador é incrível, uma
-              máquina que você pode criar o que você imaginar e o único limite é
-              o hardware, a possibilidade de criar novos mundos,
-              <em>resolver problemas</em> e <em>inovar</em> me deixou muito
-              animado! E decidi que iria aprender
+              Então, um dia eu
+              estava olhando para o computador, imerso em meus pensamentos, até
+              que tive uma epifania de como o computador é surreal, uma
+              máquina da qual você pode criar o que imaginar sendo o único limite 
+              o hardware, a possibilidade de <em>criar</em> novos mundos,
+              <em>resolver problemas</em> com <em>automação</em> e 
+              <em>inovar</em> me deixaram muito animado! Decidi naquele
+              momento que daria uma chance e aprender
               <em>desenvolvimento de software</em> apenas por diversão. Acabei
               me <em>apaixonando</em> e perdendo a noção do tempo enquanto
-              <em>estudava</em>.
+              estudava.
             </p>
 
             <p>
-              Como sempre me considerei uma <em>pessoa criativa</em> e me
-              considero uma <em>artista</em>, achei que era a área perfeita para
-              mim, se encaixava perfeitamente nas minhas características e
+              Como sempre fui considerado uma <em>pessoa criativa</em> e me
+              considero um <em>artista</em>, achei que era a área ideal para
+              mim, uma mistura de humanas com exatas, 
+              se encaixava perfeitamente nas minhas características e
               objetivos. Por exemplo, poder
-              <em>trabalhar de qualquer lugar</em> do mundo, poder trabalhar
-              viajando, poder criar algo grandioso apenas com um computador e
-              acesso à internet, entre infinitas outras possibilidades.
+              <em>trabalhar de qualquer lugar do mundo</em>, poder trabalhar
+              enquanto estiver viajando, aproveitando a vida, e poder criar algo
+              grandioso apenas com um computador e acesso à internet, como fizeram
+              algumas pessoas que fundaram suas empresas de uma garagem e até mesmo
+              do próprio quarto. Um universo de <em>infinitas possibilidades</em>.
             </p>
 
             <p>
-              Sou <em>altamente focado</em>,
-              <em>proativo</em>,<em>compreensivo</em>, <em>divertido</em>,
-              <em>inteligente</em> e <em>ambicioso</em>. Eu sigo fortemente a
-              <em>filosofia estóica</em> da qual me ajuda a lidar com a vida de
-              uma forma mais leve. Coloco pensamento e ação alinhados a um
+              Eu sou <em>altamente focado</em>,
+              <em>proativo</em>, <em>compreensivo</em>, <em>divertido</em>,
+              <em>ambicioso</em> e <em>inteligente</em>. Sigo fortemente a
+              <em>filosofia estóica</em>, da qual me ajuda a lidar com a vida de
+              uma forma mais leve e equilibrada. 
+              Coloco pensamento e ação alinhados a um
               <em>bem comum</em>, que é a <em>virtude estóica da Justiça</em>. Eu
               me liberto das distrações, me concentro apenas no que está sob meu
-              controle e faço tudo como se fosse meu último ato. Ao melhorar a mim
-              mesmo, melhoro o mundo.
+              controle e faço tudo como se fosse meu último ato, entregando o meu
+              melhor. Ao melhorar a mim mesmo, estou melhorando o mundo.
             </p>
 
             <h3>Porque Heitor The Wizard?</h3>
@@ -475,33 +480,38 @@ async function main() {
             <p>
               Acredito que os computadores são algo mágico, um artefato que
               manipula energia como desejamos, permitindo-nos expressar nossa
-              imaginação, onde tudo é possível. Uma caixa mágica na qual nos
-              permite interagir com outras pessoas do outro lado do mundo em tempo
+              imaginação, onde tudo é possível. Eles nos permitem
+              interagir com outras pessoas do outro lado do mundo em tempo
               real, registrar nossos melhores momentos com entes queridos e,
               ainda, lembrar a voz e a imagem daqueles que já amamos e que se
-              foram. É surreal como pegamos elementos da natureza e os
-              transformamos nessa "caixa" 100% lógica... <br />Portanto, acredito
-              que as pessoas que criam esses universos, jogos, programas,
-              aplicativos e todas essas obras de arte são
-              <em>magos modernos</em>. E eu sou um deles! :)
+              foram. <em>Facilitam nossas vidas</em> no dia a dia, auxiliam pessoas 
+              debilitadas. Eles nos ajudam
+              a curar doenças, a <em>resolver problemas</em> que levariam
+              anos e anos para serem resolvidos sem eles. É a <em>criação mais importante
+              da humanidade</em>.
+              É surreal como pegamos elementos da natureza e os
+              transformamos nessa "caixinha" 100% lógica.
+              Portanto, acredito que as pessoas que manipulam esse artefato, estão,
+              na verdade, manipulando energia, criando novos universos, programas,
+              aplicativos, jogos, etc. Incríveis <em>obras de arte</em>. Essas pessoas
+              para mim são como os <em>magos</em> fazendo mágicas e feitiços. 
+              E eu sou uma delas!
             </p>
 
             <h3>Minha Missão</h3>
 
             <p>
-              Eu <em>procuro</em> adquirir <em>experiência</em> aplicando
+              Eu procuro adquirir <em>experiência</em> aplicando
               desenvolvimento de software em tudo que eu estiver trabalhando,
-              <em>trazendo inovação, automação, valor e solução de problemas</em>.
-              Eu gostaria de entender como uma grande empresa funciona, como
-              <em>trabalhar em equipe com pessoas capacitadas</em> que eu posso
-              <em>aprender</em> e <em>evoluir</em> ao máximo,
+              <em>trazendo inovação, automação, solução de problemas e valor</em>.
+              Eu quero entender como uma grande companhia funciona, como é
+              <em>trabalhar em equipe com pessoas capacitadas</em> das quais posso
+              <em>aprender</em> e <em>evoluir</em>,
               <em>melhorando minhas habilidades</em> em lidar com pessoas,
-              <em
-                >negociações, resolução de problemas e desenvolvimento de
-                software</em
-              >. Quero <em>deixar um legado de grande valor</em> onde quer que eu
+              <em>negociações</em>, <em>solução de problemas</em> e <em>desenvolvimento de 
+              software</em>. Quero <em>deixar um legado de grande valor</em> onde quer que eu
               vá. Só assim poderei começar meu próprio negócio que até agora não
-              faço ideia do que vai ser... :(
+              faço ideia do que será... :(
             </p>
           </div>
         </section>
@@ -556,7 +566,7 @@ async function main() {
       </footer>
     `;
 
-    // calling main() again to reset the events of the page
+    // calling main() again to reset the page's events
     await main();
   }
 
